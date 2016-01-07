806df9994c6c21cb52dbbec59574210721436957
Michael P. Gerlek <mpg@flaxen.com>
2016-01-07 17:16:18 -0500

    move everything into one step

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,25 @@ popd > /dev/null
 export GOPATH=$base/gogo
 mkdir -p $GOPATH
 
+###
+
 go get github.com/venicegeo/pz-uuidgen
 
 go test -v github.com/venicegeo/pz-uuidgen
 
 go install github.com/venicegeo/pz-uuidgen
+
+###
+
+ls -R $GOPATH
+
+exe=uuidgen
+
+# gather some data about the repo
+source $base/vars.sh
+
+# do we have this artifact in s3? If not, upload it.
+aws s3 ls $S3URL || aws s3 cp $exe $S3URL
+
+# success if we have an artifact stored in s3.
+aws s3 ls $S3URL
