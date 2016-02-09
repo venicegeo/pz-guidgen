c722869c3a99a0ca587b4835380b4be7e022d4ef
Michael P. Gerlek <mpg@flaxen.com>
2016-02-09 15:30:12 -0500

    made debuggable

--- client/mock_uuidgen_service.go
+++ client/mock_uuidgen_service.go
@@ -2,33 +2,41 @@ package client
 
 import (
 	"github.com/venicegeo/pz-gocommon"
+	"fmt"
 )
 
 type MockUuidGenService struct {
-	name    string
-	address string
+	name      string
+	address   string
+	currentId int
 }
 
 func NewMockUuidGenService(sys *piazza.System) (*MockUuidGenService, error) {
 	var _ piazza.IService = new(MockUuidGenService)
 	var _ IUuidGenService = new(MockUuidGenService)
 
-	service := &MockUuidGenService{name: piazza.PzUuidgen, address: "0.0.0.0"}
+	service := &MockUuidGenService{name: piazza.PzUuidgen, address: "0.0.0.0", currentId: 0}
 
 	sys.Services[piazza.PzUuidgen] = service
 
 	return service, nil
 }
 
-func (c MockUuidGenService) GetName() string {
-	return c.name
+func (service MockUuidGenService) GetName() string {
+	return service.name
 }
 
-func (c MockUuidGenService) GetAddress() string {
-	return c.address
+func (service MockUuidGenService) GetAddress() string {
+	return service.address
 }
 
-func (*MockUuidGenService) PostToUuids(count int) (*UuidGenResponse, error) {
+func (service *MockUuidGenService) PostToUuids(count int) (*UuidGenResponse, error) {
+
+	data := make([]string, count)
+	for i := 0; i< count; i++ {
+		data[i] = fmt.Sprintf("%d", service.currentId)
+		service.currentId++
+	}
 	m := &UuidGenResponse{Data: []string{"xxx"}}
 	return m, nil
 }
@@ -44,6 +52,10 @@ func (*MockUuidGenService) GetFromAdminSettings() (*UuidGenAdminSettings, error)
 func (*MockUuidGenService) PostToAdminSettings(*UuidGenAdminSettings) error {
 	return nil
 }
-func (*MockUuidGenService) GetUuid() (string, error) {
-	return "this-is-a-uuid", nil
+func (service *MockUuidGenService) GetUuid() (string, error) {
+	resp, err := service.PostToUuids(1)
+	if err != nil {
+		return "", err
+	}
+	return resp.Data[0], nil
 }
