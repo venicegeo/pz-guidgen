36fd2dd21022e9f1c07ca8af5fafc1530c4b6caa
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-07-11 10:31:45 -0400

    cleanup of vagrant instances

--- config/uuid-bootstrap.sh
+++ config/uuid-bootstrap.sh
@@ -3,72 +3,40 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # install openjdk-7 
-sudo apt-get purge openjdk*
-sudo apt-get -y install openjdk-7-jdk
-
-#alias list="ls -a"
+#sudo apt-get purge openjdk*
+#sudo apt-get -y install openjdk-7-jdk
  
-#get golang
+# install golang
 cd /usr/local
 sudo wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
 
-#add go to path
-#cd /etc/profile.d
-#sudo echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile.d/gosetup.sh
+# add golang to path
 echo 'export PATH=$PATH:/usr/local/go/bin' >>/home/vagrant/.bash_profile
 export PATH=$PATH:/usr/local/go/bin
-#echo $PATH
 
-echo go help.
+# go is working
+echo go help...
 go help
 
-#apt-get install git-all
-#apt-get install git htop -y -q
+# install git
 apt-get -y install git
 
-echo PRINT USER
-whoami
-
-#su - vagrant
-
-#echo PRINT USER ..................................
-#whoami
-
-#cd /etc/profile.d
-#touch addgolangtopath.sh
-#echo 'export GOPATH=/home/vagrant/workspace/gostuff' >> addgolangtopath.sh
-
-#apt-get install golang-go
-#apt-get -y install golang-go
-
-#build and start the go app
-#echo Creating new workspace directory at home directory
+# creating new workspace directory at home directory
 mkdir /home/vagrant/workspace
 cd /home/vagrant/workspace
 
-#echo Cloning pz-logger repository...
-#git clone https://github.com/venicegeo/pz-logger.git
-#cd pz-logger
-#echo Creating gogo directory within pz-logger for GOPATH
-#mkdir gogo
-
-#echo setting GOPATH...
-#export GOPATH=$(pwd -P)/workspace/pz-logger/gogo
+# setting GOPATH...
 export GOPATH=/home/vagrant/workspace/gostuff
-#echo 'export GOPATH=/home/vagrant/workspace/gostuff' >>/home/vagrant/.bash_profile
 
-#echo 'export GOPATH=$(pwd -P)/workspace/pz-logger/gogo' >>/home/vagrant/.bash_profile
-#echo Installing pz-logger go install...
-
-#echo go help..........................................
-#go help
-
-#echo getting pz-uuidgen and trying to build it.....................
+# getting pz-uuidgen and trying to build it
 go get github.com/venicegeo/pz-uuidgen
 go install github.com/venicegeo/pz-uuidgen
 
-#start the app
+# start the app
 cd /home/vagrant/workspace/gostuff/bin
+echo List of built executables:
 ls -la
-#./pz-logger
+
+echo starting pz-uuidgen...
+./pz-uuidgen
