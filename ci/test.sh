8accda04c5072355db3d21efc1fa8b73dfc9588b
mpg <mpg@flaxen.com>
2016-07-29 16:28:19 -0400

    add go deps support

--- ci/test.sh
+++ ci/test.sh
@@ -4,17 +4,28 @@ pushd `dirname $0`/.. > /dev/null
 root=$(pwd -P)
 popd > /dev/null
 
+#----------------------------------------------------------------------
+
 export GOPATH=$root/gogo
-mkdir -p $GOPATH
+mkdir -p "$GOPATH"
 
-###
+# glide expects this to already exist
+mkdir "$GOPATH"/bin "$GOPATH"/src "$GOPATH"/pkg
 
-go get github.com/stretchr/testify/suite
-go get github.com/stretchr/testify/assert
+PATH=$PATH:"$GOPATH"/bin
 
+export GO15VENDOREXPERIMENT="1"
+
+curl https://glide.sh/get | sh
+
+# get ourself, and go there
 go get github.com/venicegeo/pz-gocommon/gocommon
+cd $GOPATH/src/github.com/venicegeo/pz-gocommon
 
-go get github.com/venicegeo/pz-uuidgen/uuidgen
+glide install
+
+#----------------------------------------------------------------------
+
 go test -v -coverprofile=uuidgen.cov github.com/venicegeo/pz-uuidgen
 
 ###
