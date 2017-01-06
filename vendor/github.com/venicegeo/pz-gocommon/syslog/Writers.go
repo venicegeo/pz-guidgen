f6ea4ccae5ae072ae6c5f60b27cc736fd0981e73
mpg <mpg@flaxen.com>
2017-01-06 15:34:11 -0500

    glide-up

--- vendor/github.com/venicegeo/pz-gocommon/syslog/Writers.go
+++ vendor/github.com/venicegeo/pz-gocommon/syslog/Writers.go
@@ -42,7 +42,7 @@ type Writer interface {
 // 2 means the two latest messages, etc. The newest message is at the end
 // of the array.
 type Reader interface {
-	Read(count int) ([]*Message, error)
+	Read(count int) ([]Message, error)
 	GetMessages(*piazza.JsonPagination, *piazza.HttpQueryParams) ([]Message, int, error)
 }
 
@@ -90,10 +90,28 @@ func (w *FileWriter) Close() error {
 
 //---------------------------------------------------------------------
 
+//STDOUTWriter writes messages to STDOUT
+type STDOUTWriter struct {
+}
+
+//Writes message to STDOUT
+func (w *STDOUTWriter) Write(mssg *Message) error {
+	var _ Writer = (*FileWriter)(nil)
+	fmt.Println(mssg.String())
+	return nil
+}
+
+//Nothing to close for this writer
+func (w *STDOUTWriter) Close() error {
+	return nil
+}
+
+//---------------------------------------------------------------------
+
 // MessageWriter implements Reader and Writer, using an array of Messages
 // as the backing store
 type LocalReaderWriter struct {
-	messages []*Message
+	messages []Message
 }
 
 // Write writes the message to the backing array
@@ -101,17 +119,17 @@ func (w *LocalReaderWriter) Write(mssg *Message) error {
 	var _ Writer = (*LocalReaderWriter)(nil)
 
 	if w.messages == nil {
-		w.messages = make([]*Message, 0)
+		w.messages = make([]Message, 0)
 	}
 
-	w.messages = append(w.messages, mssg)
+	w.messages = append(w.messages, *mssg)
 
 	return nil
 }
 
 // Read reads messages from the backing array. Will only return as many as are
 // available; asking for too many is not an error.
-func (w *LocalReaderWriter) Read(count int) ([]*Message, error) {
+func (w *LocalReaderWriter) Read(count int) ([]Message, error) {
 	var _ Reader = (*LocalReaderWriter)(nil)
 
 	if count < 0 {
@@ -119,7 +137,7 @@ func (w *LocalReaderWriter) Read(count int) ([]*Message, error) {
 	}
 
 	if w.messages == nil || count == 0 {
-		return make([]*Message, 0), nil
+		return make([]Message, 0), nil
 	}
 
 	if count > len(w.messages) {
@@ -148,10 +166,14 @@ type HttpWriter struct {
 	h   piazza.Http
 }
 
-func NewHttpWriter(url string) (*HttpWriter, error) {
-	w := &HttpWriter{}
-	w.url = url
-	w.h = piazza.Http{BaseUrl: url}
+func NewHttpWriter(url string, apiKey string) (*HttpWriter, error) {
+	w := &HttpWriter{url: url}
+	w.h = piazza.Http{
+		BaseUrl: url,
+		ApiKey:  apiKey,
+		//FlightCheck: &piazza.SimpleFlightCheck{},
+	}
+
 	return w, nil
 }
 
@@ -169,10 +191,20 @@ func (w *HttpWriter) Close() error {
 	return nil
 }
 
-func (w *HttpWriter) Read(count int) ([]*Message, error) {
+func (w *HttpWriter) Read(count int) ([]Message, error) {
 	var _ Reader = (*HttpWriter)(nil)
 
-	return nil, fmt.Errorf("HttpWriter.Read() not supported")
+	format := &piazza.JsonPagination{
+		Page:    0,
+		PerPage: count,
+		SortBy:  "timeStamp",
+		Order:   "desc",
+	}
+	params := &piazza.HttpQueryParams{}
+
+	mssgs, _, err := w.GetMessages(format, params)
+	return mssgs, err
+
 }
 
 // GetMessages is only implemented for HttpWriter as it is most likely the only
@@ -301,6 +333,34 @@ func (w *ElasticWriter) SetType(typ string) error {
 	return nil
 }
 
+func (w *ElasticWriter) CreateIndex() (bool, error) {
+	if w == nil || w.Esi == nil {
+		return false, fmt.Errorf("writer not set not set")
+	}
+	exists, err := w.Esi.IndexExists()
+	if err != nil {
+		return exists, err
+	}
+	if !exists {
+		return exists, w.Esi.Create("")
+	}
+	return exists, nil
+}
+
+func (w *ElasticWriter) CreateType(mapping string) (bool, error) {
+	if w == nil || w.Esi == nil || w.typ == "" {
+		return false, fmt.Errorf("writer not set not set")
+	}
+	exists, err := w.Esi.TypeExists(w.typ)
+	if err != nil {
+		return exists, err
+	}
+	if !exists {
+		return exists, w.Esi.SetMapping(w.typ, piazza.JsonString(mapping))
+	}
+	return exists, nil
+}
+
 // SetID sets the id to write to
 func (w *ElasticWriter) SetID(id string) error {
 	if w == nil {
@@ -318,7 +378,7 @@ func (w *ElasticWriter) Close() error {
 func (w *ElasticWriter) GetMessages(*piazza.JsonPagination, *piazza.HttpQueryParams) ([]Message, int, error) {
 	return nil, 0, fmt.Errorf("ElasticWriter.GetMessages not supported")
 }
-func (w *ElasticWriter) Read(count int) ([]*Message, error) {
+func (w *ElasticWriter) Read(count int) ([]Message, error) {
 	return nil, fmt.Errorf("ElasticWriter.Read not supported")
 }
 
--- vendor/github.com/venicegeo/pz-logger/main.go
+++ vendor/github.com/venicegeo/pz-logger/main.go
@@ -42,23 +42,31 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var esi elasticsearch.IIndex = idx
-	err = pzlogger.SetupElastic(esi)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var existed bool
 
 	logWriter := syslogger.ElasticWriter{Esi: esi}
-	err = logWriter.SetType(pzlogger.LogSchema)
-	if err != nil {
+	if err = logWriter.SetType(pzlogger.LogSchema); err != nil {
 		log.Fatal(err)
 	}
-	//auditWriter := syslogger.ElasticWriter{Esi: esi}
-	//auditWriter.SetType(pzlogger.SecuritySchema)
-	syslogWriter := syslogger.SyslogdWriter{}
+	if existed, err = logWriter.CreateIndex(); err != nil {
+		log.Fatal(err)
+	}
+	if !existed {
+		log.Printf("Created index: %s", esi.IndexName())
+	}
+	if existed, err = logWriter.CreateType(pzlogger.LogMapping); err != nil {
+		log.Fatal(err)
+	}
+	if !existed {
+		log.Printf("Created type: %s", pzlogger.LogSchema)
+	}
+
+	stdOutWriter := syslogger.STDOUTWriter{}
 
 	service := &pzlogger.Service{}
-	err = service.Init(sys, []syslogger.Writer{&logWriter}, []syslogger.Writer{&syslogWriter}, esi)
+	err = service.Init(sys, []syslogger.Writer{&logWriter}, []syslogger.Writer{&stdOutWriter}, esi)
 	if err != nil {
 		log.Fatal(err)
 	}
